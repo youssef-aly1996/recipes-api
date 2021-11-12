02ee37cfb45cb10177398717118008c48ba3e5c3
youssef-aly1996 <youssef61196@gmail.com>
2021-11-12 15:28:53 +0200

    update recipe endpoint

--- main.go
+++ main.go
@@ -52,10 +52,32 @@ func listRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
 }
 
+func updateRecipeHandler(c *gin.Context)  {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+	return
+	}
+	index := -1
+	for i:=0; i<len(recipes); i++ {
+		if recipes[i].Id == id {
+			index = i
+		}
+	}
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "recipe not found"})
+		return
+	}
+	recipes[index] = recipe
+	c.JSON(http.StatusOK, recipe)
+}
+
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", newRecipeHandler)
 	router.GET("/recipes", listRecipesHandler)
-	// router.PUT("/recipes/:id", updateRecipeHandler)
+	router.PUT("/recipes/:id", updateRecipeHandler)
 	router.Run()
 }
