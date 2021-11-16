27257e9a1609559e4f5572c8318ef02c9a59a190
youssef-aly1996 <youssef61196@gmail.com>
2021-11-16 16:08:19 +0200

    fixing code

--- handlers/handler.go
+++ handlers/handler.go
@@ -4,28 +4,50 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"recipes-api/models"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type RecipeHandler struct {
+type RecipesHandler struct {
 	collection *mongo.Collection
 	ctx context.Context
 }
 
+func NewRecipesHandler(ctx context.Context, collection *mongo.Collection) *RecipesHandler {
+	return &RecipesHandler{collection: collection, ctx: ctx}
+}
 
-func newRecipeHandler(c *gin.Context) {
-	var recipe Recipe
+func (handler *RecipesHandler) ListRecipesHandler(c *gin.Context) {
+	cur, err := handler.collection.Find(handler.ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer cur.Close(handler.ctx)
+	recipes := make([]models.Recipe, 0)
+	for cur.Next(handler.ctx) {
+		var recipe models.Recipe
+		cur.Decode(&recipe)
+		recipes = append(recipes, recipe)
+		log.Println("recipes are ",recipe)
+	}
+	c.JSON(http.StatusOK, recipes)
+}
+
+func (handler *RecipesHandler) NewRecipeHandler(c *gin.Context) {
+	var recipe models.Recipe
 	if err := c.ShouldBindJSON(&recipe); err != nil {
 		c.JSON(http.StatusBadRequest ,gin.H{"error": err.Error()})
 		return
 	}
 	recipe.Id = primitive.NewObjectID()
 	recipe.PublishedAt = time.Now()
-	_, err := client.Database("demo").Collection("recipes").InsertOne(ctx, recipe)
+	_, err := handler.collection.InsertOne(handler.ctx, recipe)
 	if err != nil {
 		log.Println(err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not insert a new recipe"})
--- main.go
+++ main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"context"
 	"log"
-	"net/http"
 	"os"
-	"time"
+	"recipes-api/handlers"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -18,9 +15,7 @@ import (
 var ctx context.Context
 var err error
 var client *mongo.Client
-var recipes [] Recipe
-var listOfRecipes []interface{}
-
+var recipesHandler *handlers.RecipesHandler
 func init()  {
 	
 	// recipes = make([]Recipe, 0)
@@ -35,54 +30,16 @@ func init()  {
 		log.Fatal(err)
 	}
 	log.Println("Connected to Mongodb")
-	// collection := client.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
+	collection := client.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
+	recipesHandler = handlers.NewRecipesHandler(ctx, collection)
+
 	// insertManyResult, err := collection.InsertMany(ctx, listOfRecipes)
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
 	// log.Println("many recipes inserted", len(insertManyResult.InsertedIDs))
 }
-type Recipe struct {
-	Id primitive.ObjectID `json:"id" bson:"_id"`
-	Name string `json:"name" bson:"name"`
-	Tags []string `json:"tags" bson:"tags"`
-	Ingredients []string `json:"ingredients" bson:"ingredients"`
-	Instructions []string `json:"instructions" bson:"instructions"`
-	PublishedAt time.Time `json:"publishedAt" bson:"publishedAt"`
-}
 
-func newRecipeHandler(c *gin.Context) {
-	var recipe Recipe
-	if err := c.ShouldBindJSON(&recipe); err != nil {
-		c.JSON(http.StatusBadRequest ,gin.H{"error": err.Error()})
-		return
-	}
-	recipe.Id = primitive.NewObjectID()
-	recipe.PublishedAt = time.Now()
-	_, err := client.Database("demo").Collection("recipes").InsertOne(ctx, recipe)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not insert a new recipe"})
-		return
-	}
-	c.JSON(http.StatusOK, recipe)
-}
-
-func listRecipesHandler(c *gin.Context) {
-	cur, err := client.Database("demo").Collection("recipes").Find(ctx, bson.M{})
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	defer cur.Close(ctx)
-	recipes = make([]Recipe, 0)
-	for cur.Next(ctx) {
-		var recipe Recipe
-		cur.Decode(&recipe)
-		recipes = append(recipes, recipe)
-	}
-	c.JSON(http.StatusOK, recipes)
-}
 
 func updateRecipeHandler(c *gin.Context)  {
 // 	id := c.Param("id")
@@ -123,8 +80,8 @@ func deleteRecipeHandler(c *gin.Context)  {
 
 func main() {
 	router := gin.Default()
-	router.POST("/recipes", newRecipeHandler)
-	router.GET("/recipes", listRecipesHandler)
+	router.POST("/recipes", recipesHandler.ListRecipesHandler)
+	router.GET("/recipes", recipesHandler.NewRecipeHandler)
 	router.PUT("/recipes/:id", updateRecipeHandler)
 	router.DELETE("/recipes/:id", deleteRecipeHandler)
 	router.Run()
